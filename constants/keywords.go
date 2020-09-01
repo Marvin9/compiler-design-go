a99db1d024a59d87e1856cbb9faca2e4367944d0
Marvin9 <marvinduff97@gmail.com>
2020-09-01 23:10:28 +0530

    chore: Add keywords & operators

--- constants/keywords.go
+++ constants/keywords.go
@@ -1,7 +1,7 @@
 package constants
 
 // TOTAL_KEYWORDS - total defined keywords
-const TOTALKEYWORDS = 17
+const TOTALKEYWORDS = 32
 
 // https://www.programiz.com/c-programming/list-all-keywords-c-language
 const AUTO = "auto"
@@ -16,16 +16,33 @@ const DOUBLE = "double"
 const ELSE = "else"
 const ENUM = "enum"
 const EXTERN = "extern"
+const FLOAT = "float"
 const FOR = "for"
 const GOTO = "goto"
 const IF = "if"
 const INT = "int"
 const LONG = "long"
+const REGISTER = "register"
+const RETURN = "return"
+const SHORT = "short"
+const SIGNED = "signed"
+const SIZEOF = "sizeof"
+const STATIC = "static"
+const STRUCT = "struct"
+const SWITCH = "switch"
+const TYPEDEF = "typedef"
+const UNION = "union"
+const UNSIGNED = "unsigned"
+const VOID = "void"
+const VOLATILE = "volatile"
+const WHILE = "while"
 
 // KEYWORDS are array of keywords
-var KEYWORDS = [TOTALKEYWORDS]string{
+var KEYWORDS = []string{
 	AUTO, BREAK, CASE, CHAR, CONST, CONTINUE, DEFAULT, DO, DOUBLE,
-	ELSE, ENUM, EXTERN, FOR, GOTO, IF, INT, LONG,
+	ELSE, ENUM, EXTERN, FLOAT, FOR, GOTO, IF, INT, LONG, REGISTER,
+	RETURN, SHORT, SIGNED, SIZEOF, STATIC, STRUCT,
+	SWITCH, TYPEDEF, UNION, UNSIGNED, VOID, VOLATILE, WHILE,
 }
 
 // IsKeyword will check if given string is keyword or not
--- constants/operators.go
+++ constants/operators.go
@@ -1,16 +1,44 @@
 package constants
 
-const TOTALOPERATORS = 5
+const TOTALOPERATORS = 22
 
+// ARITHMETIC
 const PLUS = "+"
 const MINUS = "-"
 const MULTIPLICATION = "*"
 const DIVISION = "/"
 const MODULO = "%"
 
+// RELATIONAL
+const EQUAL = "=="
+const NOT_EQUAL = "!="
+const GREATER_THAN = ">"
+const LESS_THAN = ">"
+const GREATER_THAN_EQUAL = ">="
+const LESS_THAN_EQUAL = "<="
+
+// BITWISE
+const AND = "&"
+const OR = "|"
+const XOR = "^"
+const TWOS_COMPLEMENT = "~"
+const RIGHT_SHIFT = ">>"
+const LEFT_SHIFT = "<<"
+
+// ASSIGNMENT
+const ASSIGN = "="
+const ADD = "+="
+const SUBTRACT = "-="
+const MULTIPLY = "*="
+const DIVIDE = "/="
+
 // OPERATORS - list of valid operators
 var OPERATORS = [TOTALOPERATORS]string{
 	PLUS, MINUS, MULTIPLICATION, DIVISION, MODULO,
+	EQUAL, NOT_EQUAL, GREATER_THAN, LESS_THAN,
+	GREATER_THAN_EQUAL, LESS_THAN_EQUAL, AND, OR, XOR,
+	TWOS_COMPLEMENT, RIGHT_SHIFT, LEFT_SHIFT,
+	ASSIGN, ADD, SUBTRACT, MULTIPLY, DIVIDE,
 }
 
 // IsOperator will check if token is operator or not
